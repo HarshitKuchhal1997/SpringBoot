c29727941fcd2e41911b8f4787dda7fd7eb5f242
Harshit Kuchhal <harshitkuchhal1997@gmail.com>
2023-04-29 16:26:52 +0530

    Starting of Java JPA

--- iocanddi/src/main/java/com/example/iocanddi/common/TrackCoach.java
+++ iocanddi/src/main/java/com/example/iocanddi/common/TrackCoach.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TrackCoach implements Coach{
     @Override
     public String getDailyWorkout() {
--- iocanddi/src/main/java/com/example/iocanddi/rest/DemoController.java
+++ iocanddi/src/main/java/com/example/iocanddi/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.example.iocanddi.rest;
 
 import com.example.iocanddi.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     // define a private field for the dependency
-    private Coach myChoch;
+    //public Coach myChoch;
 
-
+    private Coach anotherCoach;
 
     // define a constructor for dependency injection
 
     @Autowired
-    public DemoController(Coach theCoach){
-        myChoch =theCoach;
+    public DemoController(Coach theanotherCoach){
+        System.out.println("In Constructor: "+getClass().getSimpleName());
+        anotherCoach =theanotherCoach;
     }
+
+
 
 /*
 
@@ -32,15 +36,8 @@ public class DemoController {
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
-        return myChoch.getDailyWorkout();
+        return anotherCoach.getDailyWorkout();
     }
-
-
-
-
-
-
-
 
 
 }
